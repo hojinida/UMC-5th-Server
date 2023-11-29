355695a95c417871b08c8ddaf8824a43bd66c4b2
Hojin <8528231@naver.com>
2023-11-29 09:13:57 +0900

    Feat: ErrorHandler 8주차 미션

---
+++ src/main/java/com/example/mission7/common/exception/GeneralException.java
@@ -0,0 +1,22 @@
+package com.example.mission7.common.exception;
+
+
+import com.example.mission7.common.code.BaseErrorCode;
+import com.example.mission7.common.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
